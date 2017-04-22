8d2c480c0749a8b77a9fb503a40c9bef4391edbe
MaksKulikov <kulykov.maks@gmail.com>
2017-04-22 17:28:45 +0300

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,37 @@
 import { Component } from '@angular/core';
 
-
-
 @Component({
-  selector: 'my-app',
-  templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css']
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent  {
-  list: Array<any> = [];
+    list: Array<any> = [];
 
-  addText(text: any) {
-    if (text.trim().length) {
-      this.list.push({row: text, editing: false});
-      console.log(this.list);
+    addText(text: any) {
+        if (text.trim().length) {
+            this.list.push({
+                row:     text,
+                editing: false
+            });
+            console.log(this.list);
+        }
     }
-  }
 
-  removeText(index: number){
-      this.list.splice( index, 1 );
-  }
-
-  editText (index: number, editedText: any){
-    let editedElement = {row: editedText, editing: false};
-    if (editedText == ''){
-      this.removeText(index);
-    } else {
-      this.list.splice(index, 1, editedElement);
+    removeText(index: number){
+        this.list.splice( index, 1 );
     }
-  }
 
+    editText (index: number, editedText: any){
+        let editedElement = {
+            row:     editedText,
+            editing: false
+        };
+        if (editedText == ''){
+            this.removeText(index);
+        } else {
+            this.list.splice(index, 1, editedElement);
+        }
+    }
 }
